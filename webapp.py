381252db9f383d1ebcc93f2020011b42d20a1a71
Nicholas Avenell <nicholas@istic.net>
2015-08-27 20:54:01 +0100

    New frontend

--- webapp.py
+++ webapp.py
@@ -13,6 +13,8 @@
 # Nicholas 'Aquarion' Avenell 2015
 
 from flask import Flask
+from flask import jsonify
+from flask import render_template
 
 from kettle import Kettle
 
@@ -21,25 +23,33 @@ app = Flask(__name__)
 kettle = Kettle(False, False)
 
 @app.route('/')
-@app.route('/index')
 def index():
-	kettle.update_status()
-	return "%s" % kettle.current_status()
+	return render_template('index.html', kettle=kettle.update_status())
+
+
+@app.route('/status')
+def status():
+	return jsonify(kettle.update_status())
 
 @app.route('/stop')
 def stop():
 	kettle.update_status()
 	kettle.stopboil()
-	return "Hello World"
+	return jsonify(kettle.current_status())
 
 @app.route('/start')
 def start():
 	kettle.clickboil()
-	return "Hello World"
+	return jsonify(kettle.current_status())
+
+@app.route('/warm')
+def warm():
+	kettle.togglewarm()
+	return jsonify(kettle.current_status())
 
 @app.route('/temp/<temperature>')
 def temp(temperature):
 	kettle.set_temp(temperature)
-	return "Hello World"
+	return jsonify(kettle.current_status())
 
 app.run(debug=True)
